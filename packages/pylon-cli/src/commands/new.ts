4e9221c40c377e3d5275aa43e3aef2f108628ad0
Nico Schett <nicoschett@icloud.com>
2024-07-16 08:48:26 +0200

    fix: update bunx command in new.ts

--- packages/pylon-cli/src/commands/new.ts
+++ packages/pylon-cli/src/commands/new.ts
@@ -98,7 +98,7 @@ export default async (
     // Insert the client path into the package.json file (gqty key)
     if (options.clientPath) {
       logger.info('Inserting client path into package.json')
-      await Bun.$`cd "${projectDir}" && bunx json --yes -q -I -f package.json -e 'this.pylon = this.pylon || {}; this.pylon.gqty="${options.clientPath}"'`
+      await Bun.$`cd "${projectDir}" && bunx --yes json -q -I -f package.json -e 'this.pylon = this.pylon || {}; this.pylon.gqty="${options.clientPath}"'`
 
       logger.info('Inserted client path into package.json')
 
