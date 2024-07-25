1d83f02bd63216478d7b0ff3845f216d3d5fd223
Nico Schett <nicoschett@icloud.com>
2024-07-25 13:19:16 +0200

    fix: do not exit cli when a critical error occurs in the application
    fixes #3

--- packages/pylon-cli/src/commands/develop.ts
+++ packages/pylon-cli/src/commands/develop.ts
@@ -4,6 +4,7 @@ import {$, Subprocess, spawnSync} from 'bun'
 import {fetchSchema, generateClient} from '@gqty/cli'
 import path from 'path'
 import {sfiBuildPath, sfiSourcePath} from '../constants.js'
+import {logger} from '@getcronit/pylon'
 
 export default async (options: {port: string}) => {
   const {stdout} = spawnSync(['bunx', '--yes', '-p', 'which', 'pylon-server'])
@@ -56,9 +57,9 @@ export default async (options: {port: string}) => {
       onExit(proc) {
         // Kill if exited with code 1
 
-        if (proc.exitCode === 1) {
-          process.exit(1)
-        }
+        logger.error(
+          `Pylon exited with code ${proc.exitCode}, fix the error and save the file to restart the server`
+        )
       }
     })
   }
