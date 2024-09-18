b8b2189225d8b8ae3a083e90133de6331089dd9a
Nico Schett <nicoschett@icloud.com>
2024-09-18 14:54:58 +0200

    fix: load compilerOptions from pylon tsconfig.json
    This fixes a issue where the path aliases defined in the `tsconfig.json`
    of the pylon were not being loaded when generating the schema.
    
    This was because the `compilerOptions` were defined inside the SchemaBuilder,
    and not being loaded from the `tsconfig.json` file.
    
    Now the `tsconfig.json` file is loaded and the pre-defined `compilerOptions`
    are only used as a fallback.
    
    Closes #15

--- packages/pylon-builder/src/schema/builder.ts
+++ packages/pylon-builder/src/schema/builder.ts
@@ -1,5 +1,6 @@
 import ts from 'typescript'
 import {SchemaParser} from './schema-parser'
+import path from 'path'
 
 export class SchemaBuilder {
   private program: ts.Program
@@ -11,16 +12,9 @@ export class SchemaBuilder {
   constructor(sfiFilePath: string) {
     this.sfiFilePath = sfiFilePath
 
-    this.program = ts.createProgram([this.sfiFilePath], {
-      target: ts.ScriptTarget.ESNext,
-      module: ts.ModuleKind.CommonJS,
-      strict: true,
-      esModuleInterop: true,
-      skipLibCheck: false,
-      forceConsistentCasingInFileNames: true,
-      noImplicitAny: true,
-      experimentalDecorators: true
-    })
+    const tsConfigOptions = this.loadTsConfigOptions()
+
+    this.program = ts.createProgram([this.sfiFilePath], tsConfigOptions)
 
     this.checker = this.program.getTypeChecker()
 
@@ -48,6 +42,48 @@ export class SchemaBuilder {
     }
 
     this.sfi = sfiFileDefaultExport
+  }
+
+  private loadTsConfigOptions() {
+    const defaultOptions: ts.CompilerOptions = {
+      target: ts.ScriptTarget.ESNext,
+      module: ts.ModuleKind.CommonJS,
+      strict: true,
+      esModuleInterop: true,
+      skipLibCheck: false,
+      forceConsistentCasingInFileNames: true,
+      noImplicitAny: true,
+      experimentalDecorators: true
+    }
+
+    // Find the tsconfig.json file
+    const configPath = ts.findConfigFile(
+      path.dirname(this.sfiFilePath), // Directory to start searching from
+      ts.sys.fileExists, // Function to check if a file exists
+      'tsconfig.json' // File name to search for
+    )
+
+    if (!configPath) {
+      console.log('Could not find tsconfig.json')
+      return defaultOptions
+    }
+
+    // Read the tsconfig.json file
+    const configFile = ts.readConfigFile(configPath, ts.sys.readFile)
+
+    if (configFile.error) {
+      console.log('Could not read tsconfig.json', configFile.error)
+      return defaultOptions
+    }
+
+    // Parse the tsconfig.json file
+    const parsedConfig = ts.parseJsonConfigFileContent(
+      configFile.config,
+      ts.sys,
+      path.dirname(configPath)
+    )
+
+    return parsedConfig.options
   }
 
   public build() {
