8992a511027643d6915b1b04591ec5ae5bb255b3
Jan-Emig <jan.emig@pm.me>
2024-11-11 13:51:29 +0100

    docs: Catch bugs of nextra's useTheme hook

--- docs/components/ui/hover-border-gradient.tsx
+++ docs/components/ui/hover-border-gradient.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState, useEffect, useRef} from 'react'
+import React, {useState, useEffect} from 'react'
 import {motion} from 'framer-motion'
 import {useTheme} from 'nextra-theme-docs'
 
@@ -29,6 +29,12 @@ export function HoverBorderGradient({
 
   const {resolvedTheme} = useTheme()
 
+  // Usually, we wouldnt need this, but the useTheme hook from nextra-theme-docs seem to have (a) bug(s)
+  const theme =
+    resolvedTheme && resolvedTheme === 'system'
+      ? 'dark'
+      : resolvedTheme ?? 'dark'
+
   const rotateDirection = (currentDirection: Direction): Direction => {
     const directions: Direction[] = ['TOP', 'LEFT', 'BOTTOM', 'RIGHT']
     const currentIndex = directions.indexOf(currentDirection)
@@ -37,15 +43,6 @@ export function HoverBorderGradient({
       : (currentIndex + 1) % directions.length
     return directions[nextIndex]
   }
-
-  // const movingMap: Record<Direction, string> = {
-  //   TOP: 'radial-gradient(20.7% 50% at 50% 0%, #00b7ff 0%, rgba(255, 255, 255, 0) 100%)',
-  //   LEFT: 'radial-gradient(16.6% 43.1% at 0% 50%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)',
-  //   BOTTOM:
-  //     'radial-gradient(20.7% 50% at 50% 100%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)',
-  //   RIGHT:
-  //     'radial-gradient(16.2% 41.199999999999996% at 100% 50%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)'
-  // }
 
   const movingMap: Record<'dark' | 'light', Record<Direction, string>> = {
     dark: {
@@ -67,7 +64,7 @@ export function HoverBorderGradient({
   }
 
   const highlight =
-    resolvedTheme === 'dark'
+    theme === 'dark'
       ? 'radial-gradient(75% 181.15942028985506% at 50% 50%, #3275F8 0%, rgba(255, 255, 255, 0) 100%)'
       : 'radial-gradient(75% 181.15942028985506% at 50% 50%, #3275F8 0%, rgba(255, 255, 255, 1) 100%)'
 
@@ -110,8 +107,8 @@ export function HoverBorderGradient({
         initial={{background: movingMap[direction]}}
         animate={{
           background: hovered
-            ? [movingMap[resolvedTheme][direction], highlight]
-            : movingMap[resolvedTheme][direction]
+            ? [movingMap[theme][direction], highlight]
+            : movingMap[theme][direction]
         }}
         transition={{ease: 'linear', duration: duration ?? 1}}
       />
