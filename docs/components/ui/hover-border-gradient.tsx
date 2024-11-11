e69d12014ec09b30fbbeb3833e501358ae428ae3
Jan-Emig <jan.emig@pm.me>
2024-11-11 13:38:37 +0100

    docs: Improve border gradient button on light mode

--- docs/components/ui/hover-border-gradient.tsx
+++ docs/components/ui/hover-border-gradient.tsx
@@ -1,7 +1,8 @@
 'use client'
 import React, {useState, useEffect, useRef} from 'react'
-
 import {motion} from 'framer-motion'
+import {useTheme} from 'nextra-theme-docs'
+
 import {cn} from '@/lib/utils'
 
 type Direction = 'TOP' | 'LEFT' | 'BOTTOM' | 'RIGHT'
@@ -26,6 +27,8 @@ export function HoverBorderGradient({
   const [hovered, setHovered] = useState<boolean>(false)
   const [direction, setDirection] = useState<Direction>('TOP')
 
+  const {resolvedTheme} = useTheme()
+
   const rotateDirection = (currentDirection: Direction): Direction => {
     const directions: Direction[] = ['TOP', 'LEFT', 'BOTTOM', 'RIGHT']
     const currentIndex = directions.indexOf(currentDirection)
@@ -35,17 +38,38 @@ export function HoverBorderGradient({
     return directions[nextIndex]
   }
 
-  const movingMap: Record<Direction, string> = {
-    TOP: 'radial-gradient(20.7% 50% at 50% 0%, #00b7ff 0%, rgba(255, 255, 255, 0) 100%)',
-    LEFT: 'radial-gradient(16.6% 43.1% at 0% 50%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)',
-    BOTTOM:
-      'radial-gradient(20.7% 50% at 50% 100%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)',
-    RIGHT:
-      'radial-gradient(16.2% 41.199999999999996% at 100% 50%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)'
+  // const movingMap: Record<Direction, string> = {
+  //   TOP: 'radial-gradient(20.7% 50% at 50% 0%, #00b7ff 0%, rgba(255, 255, 255, 0) 100%)',
+  //   LEFT: 'radial-gradient(16.6% 43.1% at 0% 50%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)',
+  //   BOTTOM:
+  //     'radial-gradient(20.7% 50% at 50% 100%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)',
+  //   RIGHT:
+  //     'radial-gradient(16.2% 41.199999999999996% at 100% 50%, hsl(0, 0%, 100%) 0%, rgba(255, 255, 255, 0) 100%)'
+  // }
+
+  const movingMap: Record<'dark' | 'light', Record<Direction, string>> = {
+    dark: {
+      TOP: 'radial-gradient(20.7% 50% at 50% 0%, #3275F8 0%, rgba(255, 255, 255, 0) 100%)',
+      LEFT: 'radial-gradient(16.6% 43.1% at 0% 50%, #3275F8 0%, rgba(255, 255, 255, 0) 100%)',
+      BOTTOM:
+        'radial-gradient(20.7% 50% at 50% 100%, #3275F8 0%, rgba(255, 255, 255, 0) 100%)',
+      RIGHT:
+        'radial-gradient(16.2% 41.199999999999996% at 100% 50%, #3275F8 0%, rgba(255, 255, 255, 0) 100%)'
+    },
+    light: {
+      TOP: 'radial-gradient(20.7% 50% at 50% 0%, #3275F8 0%, rgba(255, 255, 255, 1) 100%)',
+      LEFT: 'radial-gradient(16.6% 43.1% at 0% 50%, #3275F8 0%, rgba(255, 255, 255, 1) 100%)',
+      BOTTOM:
+        'radial-gradient(20.7% 50% at 50% 100%, #3275F8 0%, rgba(255, 255, 255, 1) 100%)',
+      RIGHT:
+        'radial-gradient(16.2% 41.199999999999996% at 100% 50%, #3275F8 0%, rgba(255, 255, 255, 1) 100%)'
+    }
   }
 
   const highlight =
-    'radial-gradient(75% 181.15942028985506% at 50% 50%, #3275F8 0%, rgba(255, 255, 255, 0) 100%)'
+    resolvedTheme === 'dark'
+      ? 'radial-gradient(75% 181.15942028985506% at 50% 50%, #3275F8 0%, rgba(255, 255, 255, 0) 100%)'
+      : 'radial-gradient(75% 181.15942028985506% at 50% 50%, #3275F8 0%, rgba(255, 255, 255, 1) 100%)'
 
   useEffect(() => {
     if (!hovered) {
@@ -62,7 +86,7 @@ export function HoverBorderGradient({
       }}
       onMouseLeave={() => setHovered(false)}
       className={cn(
-        'relative flex rounded-full border  content-center bg-black/20 hover:bg-black/10 transition duration-500 dark:bg-white/20 items-center flex-col flex-nowrap gap-10 h-min justify-center overflow-visible p-px decoration-clone w-fit',
+        'relative flex rounded-full border  content-center bg-border dark:hover:bg-white/10 transition duration-500 dark:bg-white/20 items-center flex-col flex-nowrap gap-10 h-min justify-center overflow-visible p-px decoration-clone w-fit',
         containerClassName
       )}
       {...props}>
@@ -86,8 +110,8 @@ export function HoverBorderGradient({
         initial={{background: movingMap[direction]}}
         animate={{
           background: hovered
-            ? [movingMap[direction], highlight]
-            : movingMap[direction]
+            ? [movingMap[resolvedTheme][direction], highlight]
+            : movingMap[resolvedTheme][direction]
         }}
         transition={{ease: 'linear', duration: duration ?? 1}}
       />
