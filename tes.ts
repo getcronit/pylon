a0ed6413cdc6bc293a9f8cf29e405351e0499506
Nico Schett <52858351+schettn@users.noreply.github.com>
2025-01-27 13:08:19 +0000

    save

--- tes.ts
+++
@@ -1,3 +0,0 @@
-import consola from 'consola'
-
-consola.withTag('MyAppName').error('Something Wrong.')
--- test.tsx
+++
@@ -1,8 +0,0 @@
-
-
-import { renderToString, version } from 'react-dom/server'
-import { createElement, version as reactVersion } from 'react'
-
-console.log('Version', version, reactVersion)
-
-console.log('Page', renderToString(<></>))
