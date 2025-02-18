a3a7c45380f62dc0c416599b84a97679674be459
Nico Schett <nicoschett@icloud.com>
2025-02-18 23:09:40 +0100

    extend plugin system with setup, middleware, and build functions

--- packages/pylon/src/app/index.ts
+++ packages/pylon/src/app/index.ts
@@ -1,9 +1,7 @@
-import {Hono} from 'hono'
-import {logger} from 'hono/logger'
 import {sentry} from '@hono/sentry'
+import {Hono, MiddlewareHandler} from 'hono'
 
 import {asyncContext, Env} from '../context'
-import {graphqlViewerHandler} from './handler/graphql-viewer-handler'
 
 export const app = new Hono<Env>()
 
@@ -21,12 +19,24 @@ app.use('*', async (c, next) => {
   })
 })
 
-app.use('*', logger())
-
 app.use((c, next) => {
   // @ts-ignore
   c.req.id = crypto.randomUUID()
   return next()
 })
 
-app.get('/viewer', graphqlViewerHandler)
+export const pluginsMiddleware: MiddlewareHandler[] = []
+
+const pluginsMiddlewareLoader: MiddlewareHandler = async (c, next) => {
+  for (const middleware of pluginsMiddleware) {
+    const response = await middleware(c, async () => {})
+
+    if (response) {
+      return response
+    }
+  }
+
+  return next()
+}
+
+app.use(pluginsMiddlewareLoader)
