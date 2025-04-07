4e15331b6ab886b6196e02d366e0c1ffadebcc5e
Nico Schett <52858351+schettn@users.noreply.github.com>
2025-04-07 13:28:10 +0000

    fix hydration for initial route

--- packages/pylon/src/plugins/use-pages/build/plugins/inject-app-hydration.ts
+++ packages/pylon/src/plugins/use-pages/build/plugins/inject-app-hydration.ts
@@ -18,18 +18,40 @@ export const injectAppHydrationPlugin: Plugin = {
           import {hydrateRoot} from 'react-dom/client'
           import * as client from './${pathToClient}'
           import { __PYLON_ROUTER_INTERNALS_DO_NOT_USE, __PYLON_INTERNALS_DO_NOT_USE, DevOverlay, onCaughtErrorProd, onRecoverableErrorProd, onUncaughtErrorProd } from '@getcronit/pylon/pages';
-          const {createBrowserRouter, RouterProvider} = __PYLON_ROUTER_INTERNALS_DO_NOT_USE
+          const {createBrowserRouter, RouterProvider, matchRoutes} = __PYLON_ROUTER_INTERNALS_DO_NOT_USE
           const {DataClientProvider} = __PYLON_INTERNALS_DO_NOT_USE
           import React, {useMemo} from 'react'
 
-          const router = createBrowserRouter(routes)
+          hydrate()
 
-          hydrateRoot(
-            document,
-            <DataClientProvider client={client}>
-                <RouterProvider router={router} />
-            </DataClientProvider>
-          )
+          async function hydrate() {
+            // Determine if any of the initial routes are lazy
+            const lazyMatches = matchRoutes(routes, window.location)?.filter(
+              (m) => m.route.lazy
+            );
+
+            // Load the lazy matches and update the routes before creating your router
+            // so we can hydrate the SSR-rendered content synchronously
+            if (lazyMatches && lazyMatches?.length > 0) {
+              await Promise.all(
+                lazyMatches.map(async (m) => {
+                  const routeModule = await m.route.lazy!();
+                  Object.assign(m.route, { ...routeModule, lazy: undefined });
+                })
+              );
+            }
+
+            const router = createBrowserRouter(routes)
+
+            hydrateRoot(
+              document,
+              <DataClientProvider client={client}>
+                  <RouterProvider router={router} />
+              </DataClientProvider>
+            )
+          }
+
+
           `
 
         return {
