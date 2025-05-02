a53a7e91c79b67274f1a7a31db48bfd1de26a5e3
Nico Schett <52858351+schettn@users.noreply.github.com>
2025-05-02 11:36:51 +0000

    feat: add unauthorized function to handle 401 responses with customizable messages

--- packages/pylon/src/pages/http.ts
+++ packages/pylon/src/pages/http.ts
@@ -37,3 +37,23 @@ export function forbidden(
     statusText: args?.statusText || 'Forbidden'
   })
 }
+
+export function unauthorized(
+  message = 'Unauthorized',
+  args?: {
+    statusText?: string
+    returnText?: string
+    returnUrl?: string
+  }
+): never {
+  const data = {
+    message,
+    returnText: args?.returnText,
+    returnUrl: args?.returnUrl
+  }
+
+  throw new Response(JSON.stringify(data), {
+    status: 401,
+    statusText: args?.statusText || 'Unauthorized'
+  })
+}
