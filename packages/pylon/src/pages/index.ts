c54db39f1e89209b2695a776edc336948f341eed
Nico Schett <52858351+schettn@users.noreply.github.com>
2025-07-04 12:30:55 +0000

    fix: export LayoutProps

--- packages/pylon/src/pages/index.ts
+++ packages/pylon/src/pages/index.ts
@@ -3,7 +3,11 @@ export * as __PYLON_INTERNALS_DO_NOT_USE from './internals'
 export {Image} from './image'
 export {Link} from './link'
 
-export {type PageProps, type PageData} from '@/plugins/use-pages'
+export {
+  type PageProps,
+  type PageData,
+  type LayoutProps
+} from '@/plugins/use-pages'
 export * from '@/components/dev-overlay'
 export {default as GlobalErrorPage} from '@/components/global-error-page'
 export {StatusPage} from '@/components/status-page'
