ecfcc5c0c78ba90169103e188d63b3c2d0004cd8
Nico Schett <nico.schett@cronit.io>
2026-03-17 12:33:04 +0100

    fix: move `MetadataRoute` export from main package to pages

--- packages/pylon/src/index.ts
+++ packages/pylon/src/index.ts
@@ -15,7 +15,7 @@ export {createDecorator} from './create-decorator.js'
 export {getResolveInfo, ServiceError} from './define-pylon.js'
 export {getEnv} from './get-env.js'
 export {authMiddleware, requireAuth, useAuth} from './plugins/use-auth/index.js'
-export {MetadataRoute, usePages} from './plugins/use-pages/index.js'
+export {usePages} from './plugins/use-pages/index.js'
 export {pylonApp as app}
 
 import {app as pylonApp} from './app/index.js'
--- packages/pylon/src/pages/index.ts
+++ packages/pylon/src/pages/index.ts
@@ -1,18 +1,19 @@
 export * as __PYLON_ROUTER_INTERNALS_DO_NOT_USE from 'react-router'
 export * as __PYLON_INTERNALS_DO_NOT_USE from './internals'
 
-export {useRouteData, useRouteDataRefetch} from './internals'
 export {Image} from './image'
+export {useRouteData, useRouteDataRefetch} from './internals'
 export {Link} from './link'
 
-export {
-  type PageProps,
-  type PageData,
-  type LayoutProps
-} from '@/plugins/use-pages'
 export * from '@/components/dev-overlay'
 export {default as GlobalErrorPage} from '@/components/global-error-page'
 export {StatusPage} from '@/components/status-page'
+export {
+  type LayoutProps,
+  type MetadataRoute,
+  type PageData,
+  type PageProps
+} from '@/plugins/use-pages'
 
 import '../globals.css'
 
